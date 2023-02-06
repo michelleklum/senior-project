27c0aaefeb03f86556f3b724d7e1f040d0fff8e7
mlum123 <michellelum123@gmail.com>
2023-02-06 15:50:04 -0800

    Attempt to fix bug where windows cannot be opened after being closed on deployed GitHub Pages site

--- src/components/Computer/Desktop/CustomWinBox.js
+++ src/components/Computer/Desktop/CustomWinBox.js
@@ -11,22 +11,26 @@ function CustomWinBox(props) {
   const winBoxStates = useSelector(selectWinBoxStates);
   const dispatch = useDispatch();
 
+  const handleClose = (force, appName) => {
+    // props.innerRef.current.state.closed is necessary for a WinBox that begins in the OPEN state,
+    // if it appears on a page rendered after a previous page.
+    // Because in such a case, the WinBox will actually have been rendered on the previous page,
+    // (even though we did not actually place the WinBox component on that previous page).
+    // So, when the page changes from the previous page to the current page where we actually
+    // placed the WinBox, the WinBox will attempt to change its state to CLOSED.
+    // The props.innerRef.current.state.closed part prevents that from happening.
+    if (props.innerRef.current.state.closed) {
+      dispatch(closeWinBox(appName));
+      // Window-specific onclose, returns true if it does not need the default close process.
+      return true;
+    }
+  };
+
   return (
     <WinBox
       ref={props.innerRef}
       hide={winBoxStates[props.appName] === CLOSED}
-      onclose={
-        // props.innerRef.current.isClosed() is necessary for a WinBox that begins in the OPEN state,
-        // if it appears on a page rendered after a previous page.
-        // Because in such a case, the WinBox will actually have been rendered on the previous page,
-        // (even though we did not actually place the WinBox component on that previous page).
-        // So, when the page changes from the previous page to the current page where we actually
-        // placed the WinBox, the WinBox will attempt to change its state to CLOSED.
-        // The props.innerRef.current.isClosed() part prevents that from happening.
-        () =>
-          props.innerRef.current.isClosed() &&
-          dispatch(closeWinBox(props.appName))
-      }
+      onclose={(force) => handleClose(force, props.appName)}
       icon={props.icon}
       title={props.appName}
       width={props.width}
