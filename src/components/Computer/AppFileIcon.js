54bb75c766619b633446136cb8ddec5a953c9e92
mlum123 <michellelum123@gmail.com>
2023-03-06 22:46:14 -0800

    Fix bug so parent WinBox does not occlude app WinBox when app inside a WinBox is clicked

--- src/components/Computer/AppFileIcon.js
+++ src/components/Computer/AppFileIcon.js
@@ -4,9 +4,18 @@ import { openWinBox } from "../../slices/winBoxSlice";
 function AppFileIcon(props) {
   const dispatch = useDispatch();
 
-  function handleClick() {
+  function handleClick(e) {
     dispatch(openWinBox(props.appFileName));
-    props.winBoxRef.current.focus(); // Bring to front the WinBox corresponding to the clicked app / file.
+
+    // Ensure that click doesn't propagate to parent WinBox component
+    // when the app / file clicked is inside a WinBox component.
+    // Because if the click propagates to the parent, both the parent WinBox component
+    // and the WinBox component for the clicked app / file will have
+    // their z-index incremented by 1, meaning the parent WinBox component will strangely
+    // remain in front of the WinBox component for the clicked app / file.
+    e.stopPropagation();
+    // Bring to front the WinBox corresponding to the clicked app / file.
+    props.winBoxRef.current.focus();
   }
 
   return (
