6055ee99e8cf418c1a96f42094bd7ae3f366e8ad
mlum123 <michellelum123@gmail.com>
2023-03-29 14:10:13 -0700

    Flesh out README content; small updates to other WinBoxes

--- src/components/Computer/WinBoxes/ErasureWinBoxContent.js
+++ src/components/Computer/WinBoxes/ErasureWinBoxContent.js
@@ -4,13 +4,13 @@ function ErasureWinBoxContent() {
       <p>
         "The 'erased' word could persist and even multiply across other storage
         drives and devices. Erasure in that sense no longer nullifies;{" "}
-        <i>it bears witness</i>. It can be used to train algorithms or indicate
-        the intent to conceal. The changing materialities of digital inscription
-        thus ultimately entail wide-ranging political consequences."
+        <em>it bears witness</em>. It can be used to train algorithms or
+        indicate the intent to conceal. The changing materialities of digital
+        inscription thus ultimately entail wide-ranging political consequences."
       </p>
       <p>
         -- from page 14 of Dennis Tenen's{" "}
-        <i>Plain Text: The Poetics of Computation</i>
+        <cite>Plain Text: The Poetics of Computation</cite>
       </p>
     </div>
   );
--- src/components/Computer/WinBoxes/READMEWinBoxContent.js
+++ src/components/Computer/WinBoxes/READMEWinBoxContent.js
@@ -2,20 +2,69 @@ function READMEWinBoxContent() {
   return (
     <div className="custom-winbox-child text">
       <p>
+        <b>Communicative Computing:</b>
+      </p>
+      <p>
         <b>This site is about humans and machines.</b>
       </p>
-      <p>What is a computer?</p>
-      <p>AI vs. artificial communication</p>
-      <p>meaning-making metaphor machine</p>
       <p>
+        From the telegraph to HAL 9000 and now ChatGPT, it seems that, since the
+        advent of computing, there has always been a fear of the computer as an
+        intelligent being destined to bring about the end of the human race. But
+        are computers truly intelligent? <b>What is a computer?</b> And what
+        forces govern the ways in which humans interact with computers?
+      </p>
+      <p>
+        Elena Esposito pushes back against the idea of artificial intelligence,
+        instead offering the idea of <b>artificial communication</b>, building
+        on Niklas Luhmann's theory of communication as something that "comes
+        about not when somebody says something, but when somebody understands
+        that someone said something" (Esposito 21). Computers are not
+        intelligent; they do not understand the text that they display
+        on-screen, or the data that they process as input into machine learning
+        models. But they do communicate.
+      </p>
+      <p>
+        Indeed, there seems to be something inherently communicative about
+        computation: we speak of programming <em>languages</em>, each with their
+        own <em>syntax</em> and <em>grammar</em>; the <em>declaration</em> of
+        variables; and the <em>parsing</em> of data. And when we turn back the
+        clock to the first computer interfaces, we see that computers began as
+        simple command lines, telegram-type technologies that sent signals over
+        the wire for us humans to receive on the other end (Stephenson 5-6). It
+        seems that, fundamentally, computers and humans are, and have always
+        been, "communication partners" (Esposito 16).
+      </p>
+      <p>
+        <b>My thesis is this:</b> If computation is communication, the computer
+        is a meaning-making metaphor machine. And if the computer is a metaphor,
+        we can borrow words from language and literature to come to a better
+        understanding of the ways in which humans interact with digital devices.
+        But because a computer does not and <em>cannot</em> understand the data
+        that it transmits, its metaphor is incomplete without a human on the
+        receiving end. In this mode of communication, there is no meaning
+        without humanity.
+      </p>
+      <p>
+        <b>
+          For a study of the ways in which we interact with computers, what
+          better medium could there be than the computer itself?
+        </b>{" "}
+        And in computation, what device more metaphorical can there be than the
+        graphical user interface (GUI)? So, for my Media Studies capstone
+        project, I bring you this website resembling a computer desktop.
+      </p>
+      <p>
+        As you click through, you'll explore the throughline of computers as
+        communication partners from present-day GUIs, all the way back to the
+        command line.
+      </p>
+      <p>
+        Along the way, you'll learn about the mechanisms underlying our daily
+        interactions with our computers (i.e., the bits hidden under the GUI's
+        hood), while discovering a subset of my meaning-making computational
         lexicon: erasure, translation, and perhaps most importantly, dialogue.
       </p>
-      <p>
-        Along the way, you'll discover the mechanisms underlying our daily
-        interactions with our computers, from ___ to the bits hidden under the
-        hood of the GUI.
-      </p>
-      <p>Enjoy exploring!</p>
     </div>
   );
 }
