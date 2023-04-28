db9eb81011a0e9ca2f4e76448650974897394d9c
mlum123 <michellelum123@gmail.com>
2023-04-28 09:10:16 -0700

    Add Neal Stephenson quote to Punch Card

--- src/components/Computer/WinBoxes/CommunicationPartnersWinBoxContent/PunchCardWinBoxContent.js
+++ src/components/Computer/WinBoxes/CommunicationPartnersWinBoxContent/PunchCardWinBoxContent.js
@@ -6,7 +6,7 @@ function PunchCardWinBoxContent() {
       src={require("../../img/PunchCard.jpg")}
       alt={"Punch card"}
       caption={
-        "The punch card was one of the earliest mediums of computational communication. Programmers encoded messages for their machines via the presence / absence of holes on paper."
+        'The punch card was one of the earliest mediums of computational communication. Programmers encoded messages for their machines via the presence / absence of holes on paper.\n\n"We had a human/computer interface a hundred years before we had computers. When computers came into being around the time of the Second World War, humans, quite naturally, communicated with them by simply grafting them on to the already-existing technologies for translating letters into bits and vice versa: teletypes and punch card machines."\n\n-- from Neal Stephenson\'s In the Beginning… Was the Command Line'
       }
     />
   );
